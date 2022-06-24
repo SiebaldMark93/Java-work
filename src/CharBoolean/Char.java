21f5746fbaf1ba3ab364e182075aed6cc40ea2a5
Mark Siebald <siebald.mark93@gmail.com>
2022-06-23 19:02:16 -0500

    feat: char and boolean

---
+++ src/CharBoolean/Char.java
@@ -0,0 +1,16 @@
+package CharBoolean;
+
+public class Char {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicode = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+    }
+
+}
